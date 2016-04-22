c52345c4a7b4d0224eb125aaa0f0ece90d130cac
dragonly <liyilongko@gmail.com>
2016-04-23 01:52:31 +0800

    with some pretty color XD

--- Monopoly/game.cpp
+++ Monopoly/game.cpp
@@ -8,21 +8,117 @@
 
 #include "game.hpp"
 
+using std::cin;
 using std::cout;
 using std::endl;
-using std::cin;
 using std::map;
-//using monopoly::gs;
-//using monopoly::Position;
 
-void monopoly::init()
-{
+monopoly::Date::Date() {
+    d[0] = 2016; d[1] = 1; d[2] = 1;
 }
 
-void monopoly::drawMap()
-{
-//    using monopoly::gs;
-//    using
+bool monopoly::Date::isLeapYear() {
+    if (d[0] % 400 == 0)
+        return true;
+    if (d[0] % 100 == 0)
+        return false;
+    if (d[0] % 4 == 0)
+        return true;
+    return false;
+}
+
+void monopoly::Date::nextDay() {
+    if (d[2] <= 27) {
+        d[2] += 1;
+        return;
+    }
+    else if (d[1] == 2) { // Feb. and day >= 28
+        if (isLeapYear()) { // Feb has 29
+            if (d[2] == 28) {
+                d[2] += 1;
+                return;
+            }
+            d[1] = 3;
+            d[2] = 1;
+            return;
+        }
+        else { // Feb has 28
+            d[1] = 3;
+            d[2] = 1;
+        }
+    }
+    else if (d[1] == 4 || d[1] == 6 || d[1] == 9 || d[1] == 11) {
+        if (d[2] < 30) {
+            d[2] += 1;
+            return;
+        }
+        d[1] += 1;
+        d[2] = 1;
+    }
+    else {
+        if (d[2] < 31) {
+            d[2] += 1;
+            return;
+        }
+        if (d[1] != 12) {
+            d[1] += 1;
+            d[2] = 1;
+            return;
+        }
+        d[0] += 1;
+        d[1] = 1;
+        d[2] = 1;
+    }
+}
+
+ostream& monopoly::operator <<(ostream& os, monopoly::Date& date) {
+    os << date.d[0] << "年" << date.d[1] << "月" << date.d[2] << "日";
+    return os;
+}
+
+ostream& monopoly::operator <<(ostream& os, Player& player) {
+    os << player.name;
+    return os;
+}
+
+monopoly::Controler::Controler() {
+    players.push_back(Player("1P"));
+    players.push_back(Player("2P"));
+}
+
+monopoly::Player& monopoly::Controler::currentPlayer() {
+    return players[curPlayer];
+}
+
+void monopoly::Controler::nextTurn() {
+    curPlayer = (curPlayer + 1) % players.size();
+}
+
+void monopoly::Controler::eval(char* cmd) {
+    if (strcmp(cmd, "t") == 0) {
+        
+    }
+    else if (strcmp(cmd, "s") == 0) {
+        
+    }
+    else if (strcmp(cmd, "i") == 0) {
+        
+    }
+    else if (strcmp(cmd, "r") == 0) {
+        
+    }
+    else if (strcmp(cmd, "gg") == 0) {
+        
+    }
+    else {
+        
+    }
+}
+
+void monopoly::init() {
+}
+
+void monopoly::drawMap() {
     int cur;
     string name;
     for(int i = 0; i < 10; i++) {
@@ -40,25 +136,38 @@ void monopoly::drawMap()
     }
 }
 
-void monopoly::drawScreen()
-{
-    drawMap();
+void monopoly::drawPrompt() {
+    cout << "今天是" << monopoly::today << endl;
+    cout << "现在是玩家 " << GREEN << controler.currentPlayer() << NC << " 的回合, ";
+    cout << PURPLE << (controler.currentPlayer().direction ? "顺" : "逆") << NC << "时针" << endl;
+    cout << "你可以做:" << endl
+         << "t(tool) - 道具列表" << endl
+         << "s(step) - 查看前后若干步的具体信息" << endl
+         << "i(info) - 查看玩家资产信息" << endl
+         << "r(roll) - 掷骰子" << endl
+         << "gg(GG)  - 是在下输了" << endl;
 }
 
-void monopoly::gameLoop()
-{
+void monopoly::drawGame() {
+    drawMap();
+    drawPrompt();
+}
+
+void monopoly::gameLoop() {
+    char cmd[10] = "init";
     while (true) {
         system("clear");
         
-        drawScreen();
+        drawGame();
+        controler.eval(cmd);
         
+        cout << endl << PURPLE << cmd << NC << endl;
         cout << "> ";
-        cin.get();
+        cin >> cmd;
     }
 }
 
-monopoly::GameState::GameState()
-{
+monopoly::GameState::GameState() {
     int i, j, k;
     for(i = 0; i < 10; i++)
         for(j = 0; j < 20; j++)
--- Monopoly/game.hpp
+++ Monopoly/game.hpp
@@ -14,21 +14,42 @@
 #include <vector>
 #include <string>
 
+#define RED "\033[0;31m"
+#define GREEN "\033[0;32m"
+#define BROWN "\033[0;33m"
+#define BLUE "\033[0;34m"
+#define PURPLE "\033[0;35m"
+#define CYAN "\033[0;36m"
+#define LGRAY "\033[0;37m"
+#define DGRAY "\033[1;30m"
+#define LRED "\033[1;31m"
+#define LGREEN "\033[1;32m"
+#define YELLOW "\033[1;33m"
+#define LBLUE "\033[1;34m"
+#define LPURPLE "\033[1;35m"
+#define LCYAN "\033[1;36m"
+#define NC "\033[0m"
+
 using std::string;
 using std::vector;
 using std::map;
 using std::pair;
 using std::make_pair;
+using std::ostream;
 
-namespace monopoly
-{
-    void init();
-    void drawMap();
-    void drawScreen();
-    void gameLoop();
+namespace monopoly {
     
-    class Position
-    {
+    class Date {
+    public:
+        int d[3];
+        Date();
+        bool isLeapYear();
+        void nextDay();
+    };
+    
+    ostream& operator <<(ostream&, Date&);
+    
+    class Position {
     public:
         string name;
         pair<int, int> pos;
@@ -36,8 +57,7 @@ namespace monopoly
         Position() {}
     };
     
-    class GameState
-    {
+    class GameState {
     public:
         vector<Position> road = {{"land", 0, 0}, {"land", 0, 1}, {"land", 0, 2}, {"land", 0, 3}, {"land", 0, 4}, {"land", 0, 5}, {"land", 0, 6}, {"land", 0, 7}, {"land", 1, 7}, {"land", 1, 8}, {"land", 1, 9}, {"news", 1, 10}, {"land", 2, 10}, {"land", 3, 10}, {"land", 4, 10}, {"land", 4, 11}, {"land", 4, 12}, {"bank", 4, 13}, {"land", 4, 14}, {"land", 3, 14}, {"land", 2, 14}, {"land", 1, 14}, {"land", 0, 14}, {"lottery", 0, 15}, {"land", 0, 16}, {"land", 0, 17}, {"land", 0, 18}, {"coupon", 0, 19}, {"land", 1, 19}, {"land", 2, 19}, {"land", 3, 19}, {"land", 4, 19}, {"land", 5, 19}, {"land", 6, 19}, {"land", 6, 18}, {"blank", 6, 17}, {"land", 6, 16}, {"land", 7, 16}, {"land", 8, 16}, {"land", 8, 15}, {"land", 8, 14}, {"toolStore", 7, 14}, {"land", 6, 14}, {"land", 6, 13}, {"land", 6, 12}, {"land", 7, 12}, {"land", 8, 12}, {"gift", 9, 12}, {"land", 9, 11}, {"land", 9, 10}, {"land", 9, 9}, {"land", 8, 9}, {"land", 7, 9}, {"news", 6, 9}, {"land", 6, 8}, {"land", 6, 7}, {"land", 6, 6}, {"land", 5, 6}, {"land", 5, 5}, {"bank", 5, 4}, {"land", 5, 3}, {"land", 5, 2}, {"land", 4, 2}, {"land", 3, 2}, {"land", 2, 2}, {"lottery", 2, 1}, {"land", 2, 0}, {"land", 1, 0}};
         int board[10][20];
@@ -45,13 +65,52 @@ namespace monopoly
         GameState();
     };
     
-    static GameState gs;
-    static map<string, string> nameSymbolMap = {{"land", "◎ "}, {"toolStore", "道"}, {"bank", "银"}, {"news", "新"}, {"gift", "赠"}, {"blank", "空"}, {"lottery", "彩"}, {"coupon", "券"}, {"player1", "1  "}, {"player1Land", "♠ "}, {"player2", "2  "}, {"palyer2Land", "♧ "}};
-    
-    class Player
-    {
+    class Player {
+    public:
+        string name;
+        bool direction;
         
+        Player(string n, bool d = true)
+            : name(n), direction(d)
+        {
+        }
     };
+    
+    ostream& operator <<(ostream&, Player&);
+    
+    class Controler {
+    public:
+        vector<Player> players;
+        int curPlayer;
+        
+        Controler();
+        Player &currentPlayer();
+        void nextTurn();
+        void eval(char*);
+    };
+    
+    void init();
+    void drawMap();
+    void drawPrompt();
+    void drawGame();
+    void gameLoop();
+    
+    static map<string, string> nameSymbolMap = {
+        {"land", string(LGREEN)+string("◎ ")+string(NC)},
+        {"toolStore", string(BROWN)+string("道")+string(NC)},
+        {"bank", string(BLUE)+string("银")+string(NC)},
+        {"news", string(PURPLE)+string("新")+string(NC)},
+        {"gift", string(CYAN)+string("赠")+string(NC)},
+        {"blank", string(LGRAY)+string("空")+string(NC)},
+        {"lottery", string(LCYAN)+string("彩")+string(NC)},
+        {"coupon", string(LRED)+string("券")+string(NC)},
+        {"player1", string(YELLOW)+string("1  ")+string(NC)},
+        {"player1Land", string(LCYAN)+string("♠ ")+string(NC)},
+        {"player2", string(LBLUE)+string("2  ")+string(NC)},
+        {"palyer2Land", string(LBLUE)+string("♧ ")+string(NC)}};
+    static GameState gs;
+    static Date today;
+    static Controler controler;
 }
 
 #endif /* game_hpp */
