1937a0136d641a7ed5bde2e1c0a31b0ecf730067
dragonly <liyilongko@gmail.com>
2016-04-22 19:44:23 +0800

    Initial Commit

---
+++ Monopoly/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Monopoly
+//
+//  Created by Dragonly on 4/22/16.
+//  Copyright © 2016 Dragonly. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
